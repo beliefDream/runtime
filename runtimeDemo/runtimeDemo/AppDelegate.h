5a749e86d5eeca00eb4132131d1a0599d1161ef9
huangjichaolucky <zhs@zhsdeMac-mini.local>
2015-11-18 09:54:20 +0800

    初次见面

---
+++ runtimeDemo/runtimeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  runtimeDemo
+//
+//  Created by zhs on 15/11/18.
+//  Copyright (c) 2015年 zhs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ runtimeDemo/runtimeDemoTests/runtimeDemoTests.m
@@ -0,0 +1,40 @@
+//
+//  runtimeDemoTests.m
+//  runtimeDemoTests
+//
+//  Created by zhs on 15/11/18.
+//  Copyright (c) 2015年 zhs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface runtimeDemoTests : XCTestCase
+
+@end
+
+@implementation runtimeDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
